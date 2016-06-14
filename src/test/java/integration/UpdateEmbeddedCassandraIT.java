f2390a5bb463e6050a99be9454547449853ca703
Eneko Fernandez <eneko.fernandez@opencredo.com>
2016-06-14 12:51:26 +0100

    [cassandra config yaml] added config yaml for embedded cassandra

--- src/test/java/integration/UpdateEmbeddedCassandraIT.java
+++ src/test/java/integration/UpdateEmbeddedCassandraIT.java
@@ -17,16 +17,17 @@ import java.sql.DriverManager;
 public class UpdateEmbeddedCassandraIT {
 
     private static final String KEYSPACE = "liquibase";
+    private static final String CASSANDRA_CONFIGURATION_FILE = "cassandra.yaml";
 
     // Sets up an instance of Cassandra with a new empty keyspace
     @Rule
-    public CassandraCQLUnit cassandraCQLUnit = new CassandraCQLUnit(new ClassPathCQLDataSet("empty.cql", KEYSPACE));
+    public CassandraCQLUnit cassandraCQLUnit = new CassandraCQLUnit(new ClassPathCQLDataSet("empty.cql", KEYSPACE),CASSANDRA_CONFIGURATION_FILE);
 
     @Test
     public void canApplyChangelog() throws Exception {
         Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
         // Connect to the Cassandra instance setup by cassandra-unit
-        Connection con = DriverManager.getConnection("jdbc:cassandra://127.0.0.1:9142/" + KEYSPACE);
+        Connection con = DriverManager.getConnection("jdbc:cassandra://127.0.0.1:9042/" + KEYSPACE);
         // Trigger a liquibase update with a simple changelog
         Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(con));
         database.setDefaultSchemaName(KEYSPACE);
