880a35bc70f56570941329af44c130eeffe59ad3
Eneko Fernandez <eneko.fernandez@opencredo.com>
2016-06-20 09:59:41 +0100

    [poc] jdbc driver version 3.0.3

--- src/test/java/integration/UpdateCassandraSchemaIT.java
+++ src/test/java/integration/UpdateCassandraSchemaIT.java
@@ -5,6 +5,8 @@ import liquibase.Liquibase;
 import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
+import liquibase.logging.LogFactory;
+import liquibase.logging.Logger;
 import liquibase.resource.ClassLoaderResourceAccessor;
 import org.cassandraunit.CassandraCQLUnit;
 import org.cassandraunit.dataset.cql.ClassPathCQLDataSet;
@@ -14,25 +16,27 @@ import org.junit.Test;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class UpdateEmbeddedCassandraIT {
+public class UpdateCassandraSchemaIT {
+
+    private static final Logger LOG = LogFactory.getInstance().getLog(UpdateCassandraSchemaIT.class.getName());
 
     private static final String KEYSPACE = "liquibase";
     private static final String CASSANDRA_CONFIGURATION_FILE = "cassandra.yaml";
+    private static final String LIQUIBASE_CHANGELOG_FILE = "changeset/sql/changelog.sql";
+    private static final String CASSANDRA_JDBC_URL = "jdbc:cassandra://127.0.0.1:9142/";
 
     // Sets up an embedded instance of Cassandra with a new empty keyspace
     @Rule
-//    public CassandraCQLUnit cassandraCQLUnit = new CassandraCQLUnit(new ClassPathCQLDataSet("empty.cql", KEYSPACE),CASSANDRA_CONFIGURATION_FILE);
-    public CassandraCQLUnit cassandraCQLUnit = new CassandraCQLUnit(new ClassPathCQLDataSet("empty.cql", KEYSPACE));
+    public CassandraCQLUnit cassandraCQLUnit = new CassandraCQLUnit(new ClassPathCQLDataSet("empty.cql", KEYSPACE),CASSANDRA_CONFIGURATION_FILE);
 
     @Test
     public void canApplyChangelog() throws Exception {
-        Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
         // Connect to the Cassandra instance setup by cassandra-unit
-        Connection con = DriverManager.getConnection("jdbc:cassandra://127.0.0.1:9142/" + KEYSPACE);
+        Connection con = DriverManager.getConnection(CASSANDRA_JDBC_URL + KEYSPACE);
         // Trigger a liquibase update with a simple changelog
         Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(con));
         database.setDefaultSchemaName(KEYSPACE);
-        Liquibase liquibase = new Liquibase("changeset/sql/examples.sql", new ClassLoaderResourceAccessor(), database);
+        Liquibase liquibase = new Liquibase(LIQUIBASE_CHANGELOG_FILE, new ClassLoaderResourceAccessor(), database);
         Contexts contexts = new Contexts();
         liquibase.update(contexts);
     }
