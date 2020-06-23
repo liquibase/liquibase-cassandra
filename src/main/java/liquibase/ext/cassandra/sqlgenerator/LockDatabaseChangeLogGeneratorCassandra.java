7904d4ab5f6eb8549b122be355db339f340d21d4
Robert Reeves <r2@datical.com>
2020-06-23 07:12:59 -0500

    Update for latest Cassandra

--- src/main/java/liquibase/ext/cassandra/sqlgenerator/LockDatabaseChangeLogGeneratorCassandra.java
+++ src/main/java/liquibase/ext/cassandra/sqlgenerator/LockDatabaseChangeLogGeneratorCassandra.java
@@ -6,6 +6,7 @@ import liquibase.sqlgenerator.SqlGeneratorChain;
 import liquibase.sqlgenerator.SqlGeneratorFactory;
 import liquibase.sqlgenerator.core.LockDatabaseChangeLogGenerator;
 import liquibase.statement.core.LockDatabaseChangeLogStatement;
+import liquibase.statement.core.RawSqlStatement;
 import liquibase.statement.core.UpdateStatement;
 
 public class LockDatabaseChangeLogGeneratorCassandra extends LockDatabaseChangeLogGenerator {
@@ -17,18 +18,8 @@ public class LockDatabaseChangeLogGeneratorCassandra extends LockDatabaseChangeL
 
     @Override
     public Sql[] generateSql(LockDatabaseChangeLogStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
-        String liquibaseSchema = database.getLiquibaseSchemaName();
-        String liquibaseCatalog = database.getLiquibaseCatalogName();
-
-        UpdateStatement updateStatement = new UpdateStatement(liquibaseCatalog, liquibaseSchema, database.getDatabaseChangeLogLockTableName());
-        updateStatement.addNewColumnValue("LOCKED", true);
-        updateStatement.addNewColumnValue("LOCKEDBY", hostname + " (" + hostaddress + ")");
-
-        updateStatement.addNewColumnValue("LOCKGRANTED", System.currentTimeMillis());
-        updateStatement.setWhereClause(database.escapeColumnName(liquibaseCatalog, liquibaseSchema, database.getDatabaseChangeLogTableName(), "ID") + " = 1");
-
+    	RawSqlStatement updateStatement = new RawSqlStatement("UPDATE " + CassandraUtil.getKeyspace(database) + ".DATABASECHANGELOGLOCK SET LOCKED = TRUE, LOCKEDBY = '" + hostname + " (" + hostaddress + ")" + "', LOCKGRANTED = " + System.currentTimeMillis() + " WHERE ID = 1");
         return SqlGeneratorFactory.getInstance().generateSql(updateStatement, database);
-
     }
 
 }
