7b74df2dc0da2a08d8be2f3bc6c63a9d1d8b7890
Robert Reeves <r2@liquibase.com>
2020-10-09 14:40:49 -0500

    Adding functional tests for Cassandra

--- src/test/groovy/liquibase/ext/cassandra/FirstTest.groovy
+++ src/test/groovy/liquibase/ext/cassandra/FirstTest.groovy
@@ -2,6 +2,9 @@ package liquibase.ext.cassandra
 
 import liquibase.Contexts
 import liquibase.Liquibase
+import liquibase.changelog.ChangeLogHistoryService
+import liquibase.changelog.ChangeLogHistoryServiceFactory
+import liquibase.changelog.RanChangeSet
 import liquibase.integration.commandline.CommandLineUtils
 import liquibase.resource.ClassLoaderResourceAccessor
 import spock.lang.Specification
@@ -38,6 +41,18 @@ class FirstTest extends Specification {
         then:
         statusOutput.toString().trim() == "@jdbc:cassandra://localhost:9042/betterbotz;DefaultKeyspace=betterbotz is up to date";
 
+    }
+
+    def "status2"() {
+
+        when:
+        def url = "jdbc:cassandra://localhost:9042/betterbotz;DefaultKeyspace=betterbotz"
+        def defaultSchemaName = "betterbotz";
+        def database = CommandLineUtils.createDatabaseObject(new ClassLoaderResourceAccessor(), url, null, null, null, null, defaultSchemaName, false, false, null, null, null, null, null, null, null);
+        def historyService = ChangeLogHistoryServiceFactory.getInstance().getChangeLogService(database);
+        def ranChangeSets = historyService.getRanChangeSets();
+        then:
+            ranChangeSets.size() == 3;
 
     }
 
