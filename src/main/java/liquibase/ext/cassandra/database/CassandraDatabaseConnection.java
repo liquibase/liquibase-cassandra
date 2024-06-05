56a5e0fa6c88ce6ddb3fe3350a0910faad4ca1af
Maxime Wiewiora <48218208+maximevw@users.noreply.github.com>
2024-06-05 21:06:44 +0200

    Fix issue #278: add compliancemode parameter in JDBC URL if absent (#298)
    Co-authored-by: Daniel Mallorga <75833793+MalloD12@users.noreply.github.com>
    Co-authored-by: Jake Newton <jnewton@datical.com>

---
+++ src/main/java/liquibase/ext/cassandra/database/CassandraDatabaseConnection.java
@@ -0,0 +1,65 @@
+package liquibase.ext.cassandra.database;
+
+import com.ing.data.cassandra.jdbc.CassandraDriver;
+import liquibase.Scope;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+
+import java.net.URI;
+import java.sql.Driver;
+import java.util.Properties;
+
+public class CassandraDatabaseConnection extends JdbcConnection {
+
+    @Override
+    public int getPriority() {
+        return 201;
+    }
+
+    @Override
+    public void open(String url, Driver driverObject, Properties driverProperties) throws DatabaseException {
+        String jdbcUrl = url;
+
+        // When using Cassandra with com.ing.data.cassandra.jdbc.CassandraDriver, it is required to specify the
+        // compliance mode "Liquibase" in the JDBC URL. So, do it by default when it's necessary.
+        if (driverObject instanceof CassandraDriver) {
+            try {
+                boolean complianceModePresent = false;
+                final String liquibaseComplianceModeParameter = "compliancemode=Liquibase";
+                // Replace the actual protocol (jdbc:cassandra:// or any other) by a valid protocol for URI
+                // parsing (jdbc://)
+                final int protocolEndIdx = jdbcUrl.indexOf("://");
+                final String parseableUrl = "jdbc" + jdbcUrl.substring(protocolEndIdx);
+                final URI jdbcUri = URI.create(parseableUrl);
+                final String queryPart = jdbcUri.getQuery();
+                if (queryPart != null) {
+                    String[] queryParams = queryPart.split("&");
+                    for (String queryParam : queryParams) {
+                        if (liquibaseComplianceModeParameter.equals(queryParam)) {
+                            complianceModePresent = true;
+                            break;
+                        }
+                    }
+                    if (!complianceModePresent) {
+                        jdbcUrl += "&" + liquibaseComplianceModeParameter;
+                    }
+                } else {
+                    jdbcUrl += "?" + liquibaseComplianceModeParameter;
+                }
+                Scope.getCurrentScope().getLog(CassandraDatabaseConnection.class)
+                        .info("Connecting to Cassandra using: " + jdbcUrl);
+            } catch (IllegalArgumentException e) {
+                Scope.getCurrentScope().getLog(CassandraDatabaseConnection.class)
+                        .warning("Unable to check compliance mode in JDBC URL, connecting with configured URL. "
+                                + "The compliance mode might be incorrect.");
+            }
+        }
+
+        openConnection(jdbcUrl, driverObject, driverProperties);
+    }
+
+    void openConnection(String url, Driver driverObject, Properties driverProperties) throws DatabaseException {
+        super.open(url, driverObject, driverProperties);
+    }
+
+}
